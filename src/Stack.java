904d7d262f53aa1cc94d5a424a384f64c3f2d07c
angiratiwari <angiratiwari1102@gmail.com>
2024-12-19 02:12:26 +0530

    UC-1 added

--- src/Stack.java
+++ src/Stack.java
@@ -7,5 +7,17 @@ public class Stack<T> {
         list = new LinkedList<>();
     }
 
+    // UC 1: Push method (56 -> 30 -> 70)
+    public void push(T data) {
+        list.addFirst(data);  // Adds to the front (top of stack)
+    }
+
+    public void printStack() {
+        for (T item : list) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
+
 
 }
