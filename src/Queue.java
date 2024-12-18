ecc863e7982810d3596341d6a44af24d68ed0557
angiratiwari <angiratiwari1102@gmail.com>
2024-12-19 02:38:00 +0530

    UC-3 added

--- src/Queue.java
+++ src/Queue.java
@@ -6,4 +6,16 @@ public class Queue<T> {
     public Queue() {
         list = new LinkedList<>();
     }
+
+    // UC 3: Enqueue method (56 -> 30 -> 70)
+    public void enqueue(T data) {
+        list.addLast(data);  // Adds to the end (tail of queue)
+    }
+
+    public void printQueue() {
+        for (T item : list) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
 }
