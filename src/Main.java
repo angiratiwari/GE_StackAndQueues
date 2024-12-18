56db32cd88bb7f557b8effc1919a863bae62b8c4
angiratiwari <angiratiwari1102@gmail.com>
2024-12-19 02:01:49 +0530

    Initial code

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Testing Stack with UC 1 and UC 2
+        Stack<Integer> stack = new Stack<>();
+    }
+}
+
---
+++ src/Stack.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class Stack<T> {
+    private LinkedList<T> list;
+
+    public Stack() {
+        list = new LinkedList<>();
+    }
+
+
+}
